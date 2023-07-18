17075e5861a596b3be80864e05cf2f6db3d15536
SullyShah <20029618@stu.mmu.ac.uk>
2023-07-18 05:07:53 +0100

    update

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,31 +3,31 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: 'airbnb',
+  extends: "airbnb",
   overrides: [
     {
-      files: ['*.js'],
+      files: ["*.js"],
       rules: {
-        camelcase: ['error', {
-          properties: 'never',
-          allow: ['user_id', 'chat_id', 'message_id', 'first_name', 'last_name', 'session_token'], // Add 'chat_id' to the 'allow' array
+        camelcase: ["error", {
+          properties: "never",
+          allow: ["user_id", "chat_id", "message_id", "first_name", "last_name", "session_token"], // Add "chat_id" to the "allow" array
         }],
       },
     },
     {
-      files: ['*.js'],
+      files: ["*.js"],
       rules: {
-        'react/jsx-one-expression-per-line': ['off'],
+        "react/jsx-one-expression-per-line": ["off"],
       },
     },
   ],
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
   rules: {
-    camelcase: 'off',
-    'react/jsx-filename-extension': ['error', { extensions: ['.js'] }],
-    'linebreak-style': ['error', 'windows'],
+    camelcase: "off",
+    "react/jsx-filename-extension": ["error", { extensions: [".js"] }],
+    "linebreak-style": ["error", "windows"],
   },
 };
