34c4982d3af02b517d34179055f2442a7c323e35
SullyShah <20029618@stu.mmu.ac.uk>
2023-07-25 00:16:35 +0100

    Update

---
+++ components/ManageContacts.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import { View, Button, StyleSheet } from 'react-native';
+
+class ManageContactScreen extends Component {
+
+  
+  BackButton = () => {
+    this.props.navigation.goBack();
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.buttonContainer}>
+          <Button
+            title="Add Contact"
+            onPress={() =>
+              this.props.navigation.navigate('AddContact', {
+                onRefreshContacts: this.refreshContactsList,
+              })
+            }
+          />
+        </View>
+
+        <View style={styles.buttonContainer}>
+          <Button
+            title="Unblock Contact"
+            onPress={() => this.props.navigation.navigate('BlockList')}
+          />
+        </View>
+
+        <View style={styles.backButton}>
+          <Button
+            title="Back"
+            onPress={this.BackButton}
+            style={styles.backButton}
+          />
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonContainer: {
+    width: '80%',
+    marginBottom: 20,
+  },
+  backButton: {
+    width: '30%',
+    position: 'absolute',
+    bottom: 20,
+  },  
+});
+
+export default ManageContactScreen;
